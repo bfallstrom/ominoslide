2b201c6df6c76e39f2ae324ec806028ff07bee86
bfallstrom <bfallstrom@gmail.com>
2013-04-19 08:29:07 -0700

    Added a replica of Puzzle #90 from Professor Layton and the Miracle Mask (used without permission, will remove upon request) to test the intended full use of the algorithm.

--- src/com/github/bfallstrom/ominoslide/junit/LayoutFileReaderTest.java
+++ src/com/github/bfallstrom/ominoslide/junit/LayoutFileReaderTest.java
@@ -14,6 +14,7 @@ import com.github.bfallstrom.ominoslide.areastructure.WinningPosition;
 import com.github.bfallstrom.ominoslide.solver.Boards;
 import com.github.bfallstrom.ominoslide.solver.Move;
 import com.github.bfallstrom.ominoslide.tilereader.LayoutFileReader;
+import com.github.bfallstrom.ominoslide.view.BoardViewPlaintext;
 
 public class LayoutFileReaderTest {
 	private static final String filePath1 = "media/simpleLayoutReaderTest.txt";
@@ -99,5 +100,44 @@ public class LayoutFileReaderTest {
 			fail("File not found!");
 		}
 	}
-
+	
+	@Test
+	public void testLayton() {
+		generate("media/layton_5_90.txt");
+		try {
+			testIn.readAndParse();
+			System.out.println(new BoardViewPlaintext(testIn.getStartingBoard()));
+		} catch (IllegalArgumentException i) {
+			System.err.println(i.getMessage());
+			fail(i.getMessage());
+		} catch (IOException io) {
+			System.err.println(io.getMessage());
+			fail(io.getMessage());
+		} catch (RuntimeException r) {
+			System.err.println(r.getMessage());
+			fail(r.getMessage());
+		}
+		Board b = testIn.getStartingBoard();
+		WinningPosition w = testIn.getWinningBoard();
+		assertNotNull(b);
+		assertNotNull(w);
+		Boards solver = new Boards(b,w);
+		try {
+			int j = 0;
+			while(!solver.iterate()) System.out.println(j++);
+			List<Move> solution = solver.getSolution();
+			//System.out.println(solution);
+			System.out.println("Solution found in " + (solution.get(0).getDepth()+1) + " moves!");
+			System.out.println(new BoardViewPlaintext(solution.get(0).getStartingBoard()));
+			for(int i = 0; i < solution.size(); i++)
+			{
+				System.out.println();
+				System.out.println(new BoardViewPlaintext(solution.get(i).getNextBoard()));
+			}
+		} catch(RuntimeException e)
+		{
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
 }
